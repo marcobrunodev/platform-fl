5e55ac69115db79c0276ab64323cec23627184c1
Marco Bruno <marco.bruno.br@gmail.com>
2020-10-16 14:42:12 +0200

    Fim da aula 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Title from './components/Title';
 
 function App() {
   return (
-    <h1>Coisado</h1>
+    <>
+      <Title>Coiso</Title>
+      <Title>Coiso1</Title>
+      <Title>Coiso2</Title>
+    </>
   );
 }
 
