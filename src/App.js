31909b09af2108d75937aa91ac64c00dc42baccd
Marco Bruno <marco.bruno.br@gmail.com>
2020-10-16 14:57:03 +0200

    Fim da aula 3

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import Title from './components/Title';
 
 function App() {
   return (
-    <>
-      <Title>Coiso</Title>
-      <Title>Coiso1</Title>
-      <Title>Coiso2</Title>
-    </>
+    <Title>Coiso</Title>
   );
 }
 
--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './styles.css'
 
 function Title({children}) {
-  return <h1>{children}</h1>;
+  return <h1 className="title">{children}</h1>;
 }
 
 export default Title;
