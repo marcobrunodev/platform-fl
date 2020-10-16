3f207db76b2902ce22c8f1643599923e8f8a11f2
Marco Bruno <marco.bruno.br@gmail.com>
2020-10-17 01:24:10 +0200

    Fim da aula 4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Title from './components/Title';
+import TitleS from './components/TitleS';
 
 function App() {
   return (
-    <Title>Coiso</Title>
+    <>
+      <Title>Coiso</Title>
+      <TitleS>Styled-components</TitleS>
+    </>
   );
 }
 
---
+++ src/components/TitleS/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const TitleS = styled.h1`
+  color: #fff;
+  font-family: "Press Start 2P", cursive;
+  font-size: 35px;
+  text-align: center;
+  text-shadow: 4px 4px 0 #000;
+`;
+
+export default TitleS;
